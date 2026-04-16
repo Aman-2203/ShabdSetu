1be8d0254c93171c86b9c65d330b5786c19d8c51
Aman-2203 <xxxxxspacm@gmail.com>
2026-04-16 22:02:53 +0530

    seo robots & sitemap added

--- routes/page_routes.py
+++ routes/page_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, Response, request
 import logging
 
 from auth import check_trial_available, login_required
@@ -60,3 +60,54 @@ def mode_page(mode_num):
 @page_bp.route("/health")
 def health():
     return "ok", 200
+
+
+@page_bp.route('/sitemap.xml')
+def sitemap():
+    """Generate XML sitemap for search engines"""
+    base_url = "https://shabdsetu.in"
+    
+    pages = [
+        {'url': '/',              'priority': '1.0', 'changefreq': 'weekly'},
+        {'url': '/features',      'priority': '0.9', 'changefreq': 'monthly'},
+        {'url': '/pricing',       'priority': '0.8', 'changefreq': 'monthly'},
+        {'url': '/contactus',     'priority': '0.6', 'changefreq': 'monthly'},
+        {'url': '/tool',       'priority': '0.6', 'changefreq': 'monthly'},
+        {'url': '/terms&amp;conditions', 'priority': '0.3', 'changefreq': 'yearly'},
+    ]
+    
+    xml = '<?xml version="1.0" encoding="UTF-8"?>\n'
+    xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+    
+    for page in pages:
+        xml += f'  <url>\n'
+        xml += f'    <loc>{base_url}{page["url"]}</loc>\n'
+        xml += f'    <changefreq>{page["changefreq"]}</changefreq>\n'
+        xml += f'    <priority>{page["priority"]}</priority>\n'
+        xml += f'  </url>\n'
+    
+    xml += '</urlset>'
+    
+    return Response(xml, mimetype='application/xml')
+
+@page_bp.route('/robots.txt')
+def robots():
+    """Serve robots.txt for search engine crawlers"""
+    content = """User-agent: *
+Allow: /
+Allow: /features
+Allow: /pricing
+Allow: /contactus
+Disallow: /tool
+Disallow: /login
+Disallow: /send-otp
+Disallow: /verify-otp
+Disallow: /process
+Disallow: /progress/
+Disallow: /download/
+Disallow: /mode/
+
+Sitemap: {base_url}/sitemap.xml
+""".format(base_url=request.url_root.rstrip('/'))
+    
+    return Response(content, mimetype='text/plain')
